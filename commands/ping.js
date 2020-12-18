7f0ea864acc7ffd10af3240a8803384495793064
Jpuf0 <Jpufthebeast@gmail.com>
2020-12-18 02:12:17 +0000

    Started Rewrite

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (msg) {
-    const startTime = Date.now()
-    msg.channel.createMessage('🏓 Pong!').then(m => {
-        const restLatency = Date.now() - startTime
-        m.edit(`🏓 Pong! | REST: ${restLatency}ms - Gateway: ${msg._client.shards.get(0).latency}ms`)
-    })
-}
